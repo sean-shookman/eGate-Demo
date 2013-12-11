c72e5aeebed0cfa2be7bd7c8ba16b5d803ae4efe
Sean Shookman <sean-shookman@gmail.com>
2013-12-11 14:50:10 -0600

    Adding Base Project setup

---
+++ MessageService/src/main/java/com/demo/api/service/MessageService.java
@@ -0,0 +1,11 @@
+package com.demo.api.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sean
+ * Date: 12/11/13
+ * Time: 2:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MessageService {
+}
